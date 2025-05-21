0d37676599b7c1ddd5176674da3d602843ff771c
agripaa <agrieva.xananda08@gmail.com>
2025-05-21 15:24:38 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  reactStrictMode: true,
+    images: {
+        unoptimized: true, // Agar gambar bisa di-load di Netlify tanpa optimasi Next.js
+      },
 };
 
 export default nextConfig;
